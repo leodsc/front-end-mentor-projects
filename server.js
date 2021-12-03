a774cf2fbca154a770782eb7f6ca63118e8ea4e2
Leonardo <leodscarvalho@gmail.com>
2021-12-02 22:47:47 -0300

    started order summary

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const path =
+	new URL("./", import.meta.url).pathname + process.env.ORDER_SUMMARY;
+console.log(path);
+
+const PORT = process.env.PORT | 3000;
+
+app.use(express.static(path + "/public"));
+
+app.listen(PORT, () => {
+	console.log("running on port " + PORT);
+});
