4a8be3462e7c26a1dbc36411136bc60b09e6ed0f
Leonardo <leodscarvalho@gmail.com>
2021-12-07 21:20:53 -0300

    article-component

--- article-preview-component-master/public/index.js
+++ article-preview-component-master/public/index.js
@@ -1,11 +1,5 @@
 const loadSocials = () => {
 	const socialsContainer = document.createElement("div");
-	if (window.Touch) {
-		socialsContainer.className = "mobile socials-container";
-	} else {
-		socialsContainer.className = "desktop socials-container";
-	}
-
 	socialsContainer.innerHTML = `
 		<section class="socials">
 			<p>share</p>
@@ -16,27 +10,27 @@ const loadSocials = () => {
 		<div class="ballon"></div>
 	`;
 
-	let timer;
-
 	const shareButton = document.querySelector(".share");
-	shareButton.addEventListener("mouseover", () => {
-		shareButton.appendChild(socialsContainer);
-	});
-
-	shareButton.addEventListener("mouseout", () => {
-		shareButton.removeChild(socialsContainer);
-	});
-
-	shareButton.addEventListener("click", () => {
-		if (window.Touch) {
+	if (window.Touch || window.innerWidth < 1440) {
+		socialsContainer.className = "mobile socials-container";
+		shareButton.addEventListener("click", () => {
 			const info = document.querySelector(".info");
 			if (!document.querySelector(".socials-container")) {
 				info.appendChild(socialsContainer);
 			} else {
 				info.removeChild(socialsContainer);
 			}
-		}
-	});
+		});
+	} else {
+		socialsContainer.className = "desktop socials-container";
+		shareButton.addEventListener("mouseover", () => {
+			shareButton.appendChild(socialsContainer);
+		});
+
+		shareButton.addEventListener("mouseout", () => {
+			shareButton.removeChild(socialsContainer);
+		});
+	}
 };
 
 loadSocials();
