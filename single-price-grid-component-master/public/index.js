a9dd3ec2f04b4c7ab83b3e3682f210a75215a504
Leonardo <leodscarvalho@gmail.com>
2021-12-11 03:27:13 -0300

    created single-price-grid

---
+++ single-price-grid-component-master/public/index.js
@@ -0,0 +1,6 @@
+const teste = { obj: 1 };
+[1, 2, 3].forEach((number) => {
+  console.log("oi");
+});
+
+teste.a;
---
+++ single-price-grid-component-master/test.js
@@ -0,0 +1,6 @@
+const obj = {a: 1};
+[1, 2, 3].forEach((number) => {
+  console.log(number);
+});
+
+obj.a = 2;
